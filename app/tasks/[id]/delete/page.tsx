be0f0ba21957229ea30c863af825275d5d770de6
rodrigofmaceno <rferreiramaceno@gmail.com>
2024-08-25 17:17:31 -0600

    Code cleanup

--- app/tasks/[id]/delete/page.tsx
+++ app/tasks/[id]/delete/page.tsx
@@ -1,17 +1,11 @@
-
 import prisma from '@/prisma/client'
-import { Button, TextField } from '@radix-ui/themes';
-import axios from 'axios';
 import { notFound } from 'next/navigation';
 import React from 'react'
-import { useForm } from 'react-hook-form';
 import DeleteForm from '../../components/DeleteForm';
 
 interface Props {
     params : {id: string}
 }
-
-
 
 const DeleteTask = async ({params} : Props) => {
   const task = await prisma.task.findUnique({
